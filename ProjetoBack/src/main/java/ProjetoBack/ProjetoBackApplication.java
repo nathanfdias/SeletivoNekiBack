5a72fdc77db494420a2aa650285177132a3b3ba5
Nathan Dias <nathanfaria9@gmail.com>
2023-01-03 18:56:13 -0300

    Criacao Projeto

---
+++ ProjetoBack/src/main/java/ProjetoBack/ProjetoBackApplication.java
@@ -0,0 +1,13 @@
+package ProjetoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBackApplication.class, args);
+	}
+
+}
---
+++ ProjetoBack/src/test/java/ProjetoBack/ProjetoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
